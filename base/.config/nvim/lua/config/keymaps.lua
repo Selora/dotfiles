68b0db18cc96ed6a6f8254c32f9c187e3d5147dc
Selora <Selora@github.com>
2025-02-03 13:08:01 -0400

    Remapped nvim DAP debug shortcuts

--- base/.config/nvim/lua/config/keymaps.lua
+++ base/.config/nvim/lua/config/keymaps.lua
@@ -2,6 +2,10 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", "<leader>dU", function()
-  require("dapui").toggle({ reset = true })
-end, { desc = "Toggle-Reset UI" })
+
+-- stylua: ignore start
+vim.keymap.set("n", "<leader>dU", function() require("dapui").toggle({ reset = true }) end, { desc = "Toggle-Reset UI" })
+
+vim.keymap.set("n", "<leader>do", function() require("dap").step_over() end, { desc = "Step Over" })
+vim.keymap.set("n", "<leader>dO", function() require("dap").step_out() end, { desc = "Step Out" })
+-- stylua: ignore end
