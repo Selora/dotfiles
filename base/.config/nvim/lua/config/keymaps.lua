efce1964c9f87d70c28e7b7d3cd096f7e2ae918f
Selora <selora@github.com>
2025-02-28 12:41:29 -0400

    Added keybindings to quickly quit terminal mode

--- base/.config/nvim/lua/config/keymaps.lua
+++ base/.config/nvim/lua/config/keymaps.lua
@@ -4,8 +4,12 @@
 
 
 -- stylua: ignore start
+-- Debug mode
 vim.keymap.set("n", "<leader>dU", function() require("dapui").toggle({ reset = true }) end, { desc = "Toggle-Reset UI" })
-
 vim.keymap.set("n", "<leader>do", function() require("dap").step_over() end, { desc = "Step Over" })
 vim.keymap.set("n", "<leader>dO", function() require("dap").step_out() end, { desc = "Step Out" })
+
+-- Terminal mode
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", {desc = "Terminal: Quit input mode"} )
+
 -- stylua: ignore end
