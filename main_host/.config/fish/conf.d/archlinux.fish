a053c116ba52a77f6dc5613d77712565ddde793d
Selora <selora@github.com>
2025-07-16 14:32:01 -0300

    Fixing pikaur alias

--- main_host/.config/fish/conf.d/archlinux.fish
+++ main_host/.config/fish/conf.d/archlinux.fish
@@ -1,7 +1,7 @@
 if test -f /etc/arch-release
     if type -q pikaur
         function pikaur
-            env -i HOME=$HOME TERM=$TERM PATH=/usr/bin:/bin:/usr/sbin:/sbin command pikaur $argv
+            env -i HOME=$HOME TERM=$TERM PATH=/usr/bin:/bin:/usr/sbin:/sbin pikaur $argv
         end
     end
 end
