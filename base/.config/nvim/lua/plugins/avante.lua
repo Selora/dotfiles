bba0f6328fe2e0e3b582353bb1e180e596e0dfca
Selora <Selora@github.com>
2025-09-23 18:09:32 +0000

    Updated LazyVim, Mason got deprecated

--- base/.config/nvim/lua/plugins/avante.lua
+++ base/.config/nvim/lua/plugins/avante.lua
@@ -57,7 +57,7 @@ return {
             temperature = 1, -- adjust if needed
             -- max_completion_tokens = 16384,
             max_completion_tokens = 40000,
-            reasoning_effort = "medium", -- only supported for "o" models
+            reasoning_effort = "low", -- only supported for "o" models
           },
           -- max_tokens = 8192,
           -- reasoning_effort = "high", -- only supported for "o" models
@@ -85,7 +85,7 @@ return {
       "nvim-lua/plenary.nvim",
       "MunifTanjim/nui.nvim",
       --- The below dependencies are optional,
-      "echasnovski/mini.pick", -- for file_selector provider mini.pick
+      "nvim-mini/mini.pick", -- for file_selector provider mini.pick
       "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
       "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
       "ibhagwan/fzf-lua", -- for file_selector provider fzf
